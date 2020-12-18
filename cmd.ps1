7d0e9dcfab19d0110dcf48db3799d193d2da35ae
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-12-18 12:47:13 +0100

    config then remove

--- cmd.ps1
+++ cmd.ps1
@@ -6,7 +6,8 @@ if ($env:GITHUBREPO_OR_ORG.IndexOf('/') -gt 0) {
     $tokenLevel = "repos"
 };
 $removalToken = ($(Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/$tokenLevel/$env:GITHUBREPO_OR_ORG/actions/runners/remove-token" -Headers $headers -Method POST).Content | ConvertFrom-Json).token;
-.\config.cmd remove --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$removalToken" --name $env:GITHUBRUNNERNAME --unattended ;
+.\config.cmd --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$removalToken" --name $env:GITHUBRUNNERNAME --unattended ;
+.\config.cmd remove ;
 
 $registrationToken = ($(Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/$tokenLevel/$env:GITHUBREPO_OR_ORG/actions/runners/registration-token" -Headers $headers -Method POST).Content | ConvertFrom-Json).token;
 .\config.cmd --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$registrationToken" --name $env:GITHUBRUNNERNAME --unattended ;
