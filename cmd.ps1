4c48fa9eba40093b0a055e8d0843bf8c736f5a54
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-12-18 12:58:35 +0100

    replace

--- cmd.ps1
+++ cmd.ps1
@@ -6,11 +6,9 @@ if ($env:GITHUBREPO_OR_ORG.IndexOf('/') -gt 0) {
     $tokenLevel = "repos"
 };
 $removalToken = ($(Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/$tokenLevel/$env:GITHUBREPO_OR_ORG/actions/runners/remove-token" -Headers $headers -Method POST).Content | ConvertFrom-Json).token;
-.\config.cmd --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$removalToken" --name $env:GITHUBRUNNERNAME --unattended ;
-.\config.cmd remove ;
-
 $registrationToken = ($(Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/$tokenLevel/$env:GITHUBREPO_OR_ORG/actions/runners/registration-token" -Headers $headers -Method POST).Content | ConvertFrom-Json).token;
-.\config.cmd --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$registrationToken" --name $env:GITHUBRUNNERNAME --unattended ;
+
+.\config.cmd configure --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$registrationToken" --name $env:GITHUBRUNNERNAME --unattended --replace ;
 Start-Process -FilePath ".\run.cmd"
 
 $failureCount = 0
