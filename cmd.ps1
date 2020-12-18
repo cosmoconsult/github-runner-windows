167320891b562d9feebac562e3bd408f26cde813
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-12-18 12:22:43 +0100

    fix call

--- cmd.ps1
+++ cmd.ps1
@@ -7,7 +7,7 @@ if ($env:GITHUBREPO_OR_ORG.IndexOf('/') -gt 0) {
 };
 $token = ($(Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/$tokenLevel/$env:GITHUBREPO_OR_ORG/actions/runners/registration-token" -Headers $headers -Method POST).Content | ConvertFrom-Json).token;
 .\config.cmd --url "https://github.com/$env:GITHUBREPO_OR_ORG" --token "$token" --name $env:GITHUBRUNNERNAME --replace --unattended ;
-Start-Job -FilePath ".\run.cmd"
+Start-Process -FilePath ".\run.cmd"
 
 $failureCount = 0
 while ($failureCount -lt 4) {
